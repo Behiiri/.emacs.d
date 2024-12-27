0f201553f7d57135304408f0c423017815aa86f4
Behiri <behiiri@gmail.com>
2024-12-27 10:14:27 -0800

    add odin and lsp modes

--- custom.el
+++ custom.el
@@ -3,7 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(elcord diminish ivy-prescient counsel)))
+ '(package-selected-packages '(diminish ivy-prescient counsel))
+ '(package-vc-selected-packages
+   '((odin-mode :vc-backend Git :url "https://git.sr.ht/~mgmarlow/odin-mode"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -30,6 +30,26 @@
 
 (set-default-coding-systems 'utf-8)
 
+;; >>> odin
+;; (package-vc-install "https://git.sr.ht/~mgmarlow/odin-mode")
+;; Enable odin-mode and configure OLS as the language server
+(use-package odin-mode
+              :mode ("\\.odin\\'" . odin-mode)
+              :hook (odin-mode . lsp))
+
+;; Set up OLS as the language server for Odin, ensuring lsp-mode is loaded first
+(with-eval-after-load 'lsp-mode
+  (setq-default lsp-auto-guess-root t) ;; Helps find the ols.json file with Projectile or project.el
+  (setq lsp-language-id-configuration (cons '(odin-mode . "odin") lsp-language-id-configuration))
+
+  (lsp-register-client
+   (make-lsp-client :new-connection (lsp-stdio-connection "C:/Tools/ols/ols.exe")
+                    :major-modes '(odin-mode)
+                    :server-id 'ols
+                    :multi-root t))) ;; Ensures lsp-mode sends "workspaceFolders" to the server
+
+(add-hook 'odin-mode-hook #'lsp)
+;; <<< odin
 
 (use-package compile
   :init
